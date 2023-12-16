d1c57072845551fd0c7eb0a4233686406175fdcb
Renovatoo <kamesash251@gmail.com>
2023-12-16 22:51:39 +1000

    0.2

--- KnightRunner/game.py
+++ KnightRunner/game.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import sys
-from scripts.entities import EntityPhysx
-from scripts.utilities import load_image, load_images
+from scripts.entities import EntityPhysx, Player
+from scripts.utilities import load_image, load_images, Animation
 from scripts.tilemap import Tilemap
 
 # Далее идёт всё про принципам ООП
@@ -22,17 +22,21 @@ class Game:
         # создаём фиксированную частоту кадров чтобы игровые тики были равномерны и не перегружали процессор
 
         self.assets = { # создаём shortcuts для разных типов тайлов
-            'player': load_image('entities/player.png'),
+            'player': load_image('entities/player/player.png'),
             'enemies': load_images('entities/enemies'),
             'decor': load_images('decor'),
             'terrain': load_images('terrain'),
             'blocks': load_images('blocks'),
             'misc': load_images('misc'),
             'background': load_image('background.png'),
+            'player/idle': Animation(load_images('entities/player/idle'), img_dur=6),
+            'player/run': Animation(load_images('entities/player/run'), img_dur=7),
+            'player/jump': Animation(load_images('entities/player/jump')),
+            'player/climb': Animation(load_images('entities/player/climb'), img_dur=4),
         } # в папке из load images должны быть только png с int-именами.
 
         self.movement = [False, False]
-        self.player = EntityPhysx(self, 'player', (50, 50), (15, 15))
+        self.player = Player(self, (50, 50), (15, 15))
         self.tilemap = Tilemap(self, tile_size=16)
         self.scroll = [0, 0] # scroll будет относительно экрана, т.е. это реализует камеру
 
@@ -41,9 +45,9 @@ class Game:
         while True: # бесконечный цикл чтобы игра не закрывалась
             self.display.blit(self.assets['background'], (0, 0))
 
-            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 18
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 20
             # сдвиг камеры по X = текущая позиция игрока - "позиция" дисплея (делённая на два иначе камера улетит) - где мы уже находимя
-            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 18
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 20
             render_scroll = (int(self.scroll[0]), int(self.scroll[1])) # избавление от дергания камеры
 
             self.tilemap.render(self.display, offset=render_scroll)
@@ -74,7 +78,7 @@ class Game:
             self.screen.blit(pg.transform.scale(self.display, self.screen.get_size()), (0, 0)) 
             # Делаем так чтобы маленькая область растянулась на весь экран и от этого текстурки стали больше.
             pg.display.update() # обновляем экран чтобы видеть изменения
-            self.clock.tick(60) # устанавливаем фреймрэйт в 60 кадров/c
+            self.clock.tick(70) # устанавливаем фреймрэйт в 60 кадров/c
 
 
 Game().run()
--- KnightRunner/scripts/entities.py
+++ KnightRunner/scripts/entities.py
@@ -10,10 +10,20 @@ class EntityPhysx: # класс который будет отвечать за 
         self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
         # это изначальные статусы collision чтобы сбрасывать ускорение свободного падения
 
+        self.action = ''
+        self.flip = False
+        self.set_action('idle')
+
 
     def rect(self):
         return pg.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
     
+
+    def set_action(self, action):
+        if action != self.action:
+            self.action = action
+            self.animation = self.game.assets[self.type + '/' + self.action].copy()
+
 
     def update(self, tilemap, movement=(0, 0)):
         self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
@@ -46,6 +56,11 @@ class EntityPhysx: # класс который будет отвечать за 
                     self.collisions['up'] = True    
                 self.pos[1] = entity_rect.y
         
+        if movement[0] > 0:
+            self.flip = False
+        if movement[0] < 0:
+            self.flip = True
+
         self.velocity[1] = min(5, self.velocity[1] + 0.1) 
         # min чтобы не было бесконечного ускорения падения, т.е. 5 - это максимальная "скорость" падения
         
@@ -53,8 +68,30 @@ class EntityPhysx: # класс который будет отвечать за 
             self.velocity[1] = 0
         # просто сбрасываем ускорение свободного падения когда сталкиваемся с землёй или потолком
 
+        self.animation.update()
 
-    def render(self, surf, offset=(0, 0)):
-        surf.blit(self.game.assets['player'], (self.pos[0] - offset[0], self.pos[1] - offset[1]))
 
-        
+    def render(self, surf, offset=(0, 0)):                          # false для отключение переворота по Y
+        surf.blit(pg.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0], self.pos[1] - offset[1]))
+
+
+class Player(EntityPhysx):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'player', pos, size)
+        self.air_time = 0
+
+
+    def update(self, tilemap, movement=(0, 0)):
+        super().update(tilemap, movement=movement)
+
+        self.air_time += 1
+        if self.collisions['down']:
+            self.air_time = 0
+
+        if self.air_time > 4:
+            self.set_action('jump')
+        elif movement[0] != 0:
+            self.set_action('run')
+        else:
+            self.set_action('idle')
+
